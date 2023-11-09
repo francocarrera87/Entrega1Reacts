e997ea142908c8700b4ff444639c6790cb8d30f3
francocarrera87 <carrerafranco02@gmail.com>
2023-11-08 21:18:21 -0300

    commit1

--- src/components/NavBarComponent/NavBarcomponent.jsx
+++ src/components/NavBarComponent/NavBarcomponent.jsx
@@ -7,21 +7,21 @@ const NavBarComponent =()  =>{
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">React-Kaster</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Prueba1</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                Prueba2
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Prueba3</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Prueba4
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
